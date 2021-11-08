20242ef704542f2532b778ee0631db2721514ac6
Kyun <kyun@iamkyun.com>
2021-11-08 16:42:59 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(title: const Text(_title)),
           body: Container(
-              margin: const EdgeInsets.all(12.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 30, horizontal: 12.0),
               child: const MyStatefulWidget()),
         ));
   }
@@ -42,43 +43,100 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        InputDecorator(
-          decoration: InputDecoration(
-              labelText: '代码',
-              labelStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              value: _selectedCode,
-              isDense: true,
-              style: const TextStyle(color: Colors.black54, fontSize: 16.0),
-              onChanged: (value) => {
-                setState(() {
-                  _selectedCode = value;
-                })
-              },
-              items: _codes.entries.map((e) {
-                return DropdownMenuItem<String>(
-                  value: e.key,
-                  child: Text(e.value),
-                );
-              }).toList(),
+        Row(children: [
+          Expanded(
+              child: InputDecorator(
+            decoration: InputDecoration(
+                labelText: '代码',
+                labelStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0))),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: _selectedCode,
+                isDense: true,
+                style: const TextStyle(color: Colors.black54, fontSize: 16.0),
+                onChanged: (value) => {
+                  setState(() {
+                    _selectedCode = value;
+                  })
+                },
+                items: _codes.entries.map((e) {
+                  return DropdownMenuItem<String>(
+                    value: e.key,
+                    child: Text(e.value),
+                  );
+                }).toList(),
+              ),
             ),
-          ),
+          ))
+        ]),
+        const SizedBox(
+          height: 20,
         ),
-        const SizedBox(height: 20),
-        TextField(
-          enabled: false,
-          controller: _selectedDevice,
-          decoration: InputDecoration(
-              hintText: '点击选择蓝牙设备',
-              labelText: '蓝牙设备',
-              hintStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
-              labelStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
+        Row(
+          children: [
+            Expanded(
+                child: TextField(
+              enabled: false,
+              controller: _selectedDevice,
+              decoration: InputDecoration(
+                  labelText: '选择蓝牙设备',
+                  labelStyle:
+                      const TextStyle(color: Colors.blue, fontSize: 16.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            )),
+            Container(
+                margin: const EdgeInsets.only(left: 6),
+                height: 50,
+                child: MaterialButton(
+                    elevation: 5,
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    splashColor: Colors.blue,
+                    padding: const EdgeInsets.all(6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Text(
+                          "搜索设备",
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        ),
+                        Icon(Icons.search),
+                      ],
+                    ),
+                    onPressed: () => {debugPrint('SEARCH BLUETOOTH DEVICES')})),
+          ],
         ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: 120,
+                height: 50,
+                child: MaterialButton(
+                    elevation: 5,
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    splashColor: Colors.blue,
+                    padding: const EdgeInsets.all(6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const <Widget>[
+                        Text(
+                          "发 送",
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                        Icon(Icons.upload),
+                      ],
+                    ),
+                    onPressed: () => {debugPrint('SEND DATA')}))
+          ],
+        )
       ],
     );
   }
